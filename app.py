f6ece31c44ab9a737ec9d60a3d054f0edcb747f7
Edward J. Jinotti <ejinotti2@gmail.com>
2015-12-09 21:00:26 -0500

    Add browserify & jquery.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 from collections import namedtuple, defaultdict
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/public')
 
 Item = namedtuple('Item', 'name protein carbs fat calories')
 
@@ -28,18 +28,13 @@ def readfile(f):
     return data
 
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     with app.open_resource('fueldata.txt', 'r') as f:
         data = readfile(f)
 
     return render_template('./index.html', **data)
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(app.root_path, 'favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
 
 
 app.run(debug=True)
